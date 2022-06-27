9c312de3d252590d390fd73584ee7ffa15d759fb
Harshu8 <69030020+Harshu8@users.noreply.github.com>
2022-06-27 11:45:11 +0530

    Update ques2.js

--- ques2.js
+++ ques2.js
@@ -1,4 +1,4 @@
-// Write a program to show the current date in DD-MM-YYYY HH:MM:SS format.
+// Ques.2 : Write a program to show the current date in DD-MM-YYYY HH:MM:SS format.
 
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
